d9e9b67486a4c359bc9228e6a8a6a6b57e6674ab
Ricardo Vilaça <rmvilaca@di.uminho.pt>
2011-12-02 15:16:29 +0000

    Add  scripts/gen-TPCC.py to generate latency and throughput for TPC-C. FIX README to use derbySQL.properties as derby.properties is used to configure derby and may conflict in embedded mode.

---
+++ scripts/gen-TPCC.py
@@ -0,0 +1,66 @@
+from pyx import *
+from statlib import stats
+import sys
+
+def main(args):
+		types=["Payment", "New-Order", "Delivery","Order-Status","Stock-Level"]
+		outT=open("TPCC-throughput.dat", 'w')
+		outL=open("TPCC-latency.dat", 'w')
+		clients=[1,10,20,30,40]
+		if len(clients)!=(len(args)-1):
+			print "The size of clients it not compatible with the list of files."
+			sys.exit(0)
+		for i in range(1,len(args)):
+			fin=args[i]
+			startLine=False
+			notEndLine=True
+			throughput=0
+			mapLatency={}
+			print fin, clients[i-1]
+			for t in types:
+   				mapLatency[t]=[]
+   			mapLatency["total"]=[]
+   			f = open(fin, 'r')
+   			lines = f.readlines()
+   			for line in lines:
+   				sep=line.split('\t')
+   				if line.startswith("Transaction Number"):
+   					startLine=True
+   				elif line.startswith("Measured tpmC"):
+   					expr=line.split("=")[1]
+   					tmp1=expr.split("/")
+   					tmp2=tmp1[0].split("*")
+					throughput=float(tmp2[0])*float(tmp2[1])/float(tmp1[1])
+   				elif startLine and line=="\n":
+   						notEndLine=False
+   				elif startLine and notEndLine:
+					for t in types:
+   						if t==sep[2]:
+   							mapLatency[t].append(float(sep[3]))
+   					mapLatency["total"].append(float(sep[3]))
+   			print >>outT, clients[i-1],throughput
+   			print >>outL, clients[i-1],
+   			for t in types:
+   				print >>outL,stats.mean(mapLatency[t]),
+   			print >>outL, 	stats.mean(mapLatency["total"])	
+   			f.close()
+		outT.close()
+		outL.close()
+		inT="TPCC-throughput"
+		inL="TPCC-latency"
+		mColor=[color.cmyk.Yellow,color.cmyk.Orange ,color.cmyk.Red,color.cmyk.Violet,color.cmyk.Blue,color.cmyk.Green,color.cmyk.Sepia ,color.cmyk.Brown, color.cmyk.Gray, color.cmyk.Black]
+		gT =graph.graphxy(width=8,key=graph.key.key(pos="br", dist=0.1), x=graph.axis.linear(min=0,max=50,title="Clients"),  y=graph.axis.linear(min=0,max=2000,title="Throughput (ops/min)"))
+		gT.plot([graph.data.file(inT+".dat", x=1, y=2,title=t)],[graph.style.symbol(symbolattrs=[attr.changelist(mColor)]),graph.style.line([attr.changelist(mColor),style.linewidth.Thick])])
+		gT.writePDFfile(inT)
+		gT.writeEPSfile(inT)
+		gL =graph.graphxy(width=8,key=graph.key.key(pos="br", dist=0.1), x=graph.axis.linear(min=0,max=50,title="Clients"),  y=graph.axis.linear(title="Latency (ms)"))
+		lL=[]
+		i=2
+		for t in types:
+			lL.append(graph.data.file(inL+".dat", x=1, y=i,title=t))
+			i+=1
+		lL.append(graph.data.file(inL+".dat", x=1, y=i,title="Total"))
+		gL.plot(lL,[graph.style.symbol(symbolattrs=[attr.changelist(mColor)]),graph.style.line([attr.changelist(mColor),style.linewidth.Thick])])
+		gL.writePDFfile(inL)
+		gL.writeEPSfile(inL)
+main(sys.argv)
